da65375b687022268535a0124b3d0f24076c4395
blueskinlizard <blueskinlizard@gmail.com>
2025-02-16 19:03:26 -0600

    Updated everything, made it look nice

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+const rockButtonAi = document.querySelector("#rockButton-ai");
+const paperButtonAi = document.querySelector("#paperButton-ai");
+const scissorsButtonAi = document.querySelector("#scissorsButton-ai");
+const titleResult = document.querySelector("#titleResult");
+const playerScoreOutput = document.querySelector("#player-score");
+const computerScoreOutput = document.querySelector("#computer-score");
 let y = null;
+let playerScore = 0;
+let computerScore = 0;
+let roundsPlayed = 1;
 function aiAssign(){
     return Math.floor(Math.random() * 3)
 }
@@ -16,35 +25,45 @@ function scissorsMethod(){
     y=2;
     playGame();
 }
-function stringAssign(x){
-    switch(x){
+function stringAssign(x) {
+    switch(x) {
         case 0:
+            document.getElementById("rockButton-ai").setAttribute("id", "selectedButtonAi");
             return "rock";
-            break;
         case 1:
+            document.getElementById("paperButton-ai").setAttribute("id", "selectedButtonAi");
             return "paper";
-            break;
         case 2:
+            document.getElementById("scissorsButton-ai").setAttribute("id", "selectedButtonAi");
             return "scissors";
-            break;
     }
 }
+
+
 function calculateWin(y, t, x){
     if(y == x){
-        alert("you drew with the AI, who also picked: "+t);
+        titleResult.textContent = "You drew to the AI";
         return 2;
     }
     //same thing drawn
     if(y == 0 && x == 2 || y == 1 && x == 0 || y == 2 && x== 1){
-        alert("you win against the AI, who picked: "+t);
+        titleResult.textContent = "You won against the AI";
+        playerScore++;
         return 1;
     }
     //all win contingencies
-    alert("you lost to the AI, who picked: "+t);
+    titleResult.textContent = "You lost to the AI";
+    computerScore++;
     return 0;
     //no wins drawn or draws made, so automatic loss
 }
 function playGame(){
+    roundsPlayed++;
+    rockButtonAi.setAttribute("id", "rockButton-ai");
+    paperButtonAi.setAttribute("id", "paperButton-ai");
+    scissorsButtonAi.setAttribute("id", "scissorsButton-ai");
+    playerScoreOutput.textContent = "Player Score: " + playerScore;
+    computerScoreOutput.textContent = "Computer Score: " + computerScore;
     let x = aiAssign();
     calculateWin(y, stringAssign(x), x);
 
